d33471e507a61ab0b6aadfcdc4d0b08243e33cb2
md-arif-islam <arifislamarif344@gmail.com>
2023-08-05 11:01:34 +0600

    - chore(package.json): add @iconify/vue, sass, and uid as devDependencies - feat(App.vue): add TodoHeader component and import it - feat(App.vue): add global styles for font and container - feat(App.vue): add TodoHeader component template - feat(Todo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import TodoHeader from "./components/TodoHeader.vue";
 </script>
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&display=swap");
+
+* {
+  font-family: "Quicksand", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
