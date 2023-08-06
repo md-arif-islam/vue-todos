e7354c2f9c15fdab8ef4931e911287d8269ca295
md-arif-islam <arifislamarif344@gmail.com>
2023-08-06 11:45:48 +0600

    - fix(TodosView.vue): fix isCompleted initial value to false - feat(TodosView.vue): add button content to TodoCreator component - feat(TodosView.vue): add conditional rendering for todo list and message when there are no todos - style(TodosView.vue): add styles for todo list and

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from "vue";
 import { uid } from "uid";
+import { ref } from "vue";
 import TodoCreator from "../components/TodoCreator.vue";
 import TodoItem from "../components/TodoItem.vue";
 
@@ -9,7 +9,7 @@ const createTodo = (todo) => {
   todoList.value.push({
     id: uid(),
     todo,
-    isCompleted: null,
+    isCompleted: false,
     isEditing: null,
   });
 };
@@ -18,10 +18,16 @@ const createTodo = (todo) => {
 <template>
   <main>
     <h1>Create Todo</h1>
-    <TodoCreator @create-todo="createTodo" />
-    <ul>
+    <TodoCreator @create-todo="createTodo">
+      <template #button-content>Create</template>
+    </TodoCreator>
+    <ul class="todo-list" v-if="todoList.length > 0">
       <TodoItem v-for="todo in todoList" :todo="todo" />
     </ul>
+    <p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -37,6 +43,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
